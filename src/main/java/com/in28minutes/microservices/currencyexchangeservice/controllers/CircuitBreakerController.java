bd7c6d1ad638efbdc7b64375a75d451ff827e40f
SibuleleMboyi <smboyi2016@gmail.com>
2022-08-17 03:01:13 +0200

    playing with resilence

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controllers/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -8,27 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
-
 
 @RestController
 public class CircuitBreakerController {
 
-    
-    @Autowired(required =  false)
+    @Autowired(required = false)
     private Logger logger = LogManager.getLogger(CircuitBreakerController.class);
 
+    // @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name = "dafault", fallbackMethod = "hardcodedResponse")
+
+    // Allows a specific number of calls to a certain Micro-Service
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
     public String sampleApi() {
 
-        logger.info("Sample API call recieved");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("htt://localhost:8000/some-dummy",
-                String.class);
-        return forEntity.getBody();
+        /*
+         * logger.info("Sample API call recieved");
+         * ResponseEntity<String> forEntity = new
+         * RestTemplate().getForEntity("htt://localhost:8000/some-dummy",
+         * String.class);
+         */
+        return "forEntity.getBody()";
     }
 
-    public String hardcodedResponse(Exception exception){
+    public String hardcodedResponse(Exception exception) {
         return "fallback-response";
     }
 }
